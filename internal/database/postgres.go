a1147151787cc35683949219be435ae01a08e7fe
vadhe <aldyvadhe@gmail.com>
2026-01-25 18:00:37 +0800

    Load local .env only when not on Railway

--- internal/database/postgres.go
+++ internal/database/postgres.go
@@ -13,8 +13,11 @@ import (
 
 func OpenPostgres() (*sql.DB, error) {
 	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	if _, exists := os.LookupEnv("RAILWAY_ENVIRONMENT"); !exists {
+		err := godotenv.Load()
+		if err != nil {
+			log.Println("Note: Error loading .env file locally (expected in Railway deployment)")
+		}
 	}
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
